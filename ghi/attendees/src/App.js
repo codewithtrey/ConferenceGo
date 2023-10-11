8e66d866c7b10a34f6afbf40708aa3e309012a36
Trejon McGee <trejon.mcgee@gmail.com>
2023-10-11 15:29:19 -0500

    Completed D3: Attendees List

--- ghi/attendees/src/App.js
+++ ghi/attendees/src/App.js
@@ -1,7 +1,30 @@
-function App() {
+import Nav from './Nav';
+
+function App(props) {
   return (
-    <div>
+    <>
+    <Nav />
+    <div className="container">
+      <table className="table table-striped">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Conference</th>
+          </tr>
+        </thead>
+        <tbody>
+          {props.attendees?.map(attendee => {
+            return (
+            <tr key={attendee.href}>
+              <td>{ attendee.name }</td>
+              <td>{ attendee.conference }</td>
+            </tr>
+          );
+        })}
+        </tbody>
+      </table>
     </div>
+    </>
   );
 }
 
