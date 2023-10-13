d0381802502e6f4b06969b69d9ce1ff7fbde8b44
Trejon McGee <trejon.mcgee@gmail.com>
2023-10-13 11:00:46 -0500

    Working through D5: Making A SPA (Routing Ever.)

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -1,6 +1,8 @@
 import Nav from './Nav';
 import AttendeesList from './AttendeesList';
 import LocationForm from './LocationForm';
+import ConferenceForm from './ConferenceForm'
+import AttendConferenceForm from './AttendConferenceForm'
 
 function App(props) {
   if (props.attendees === undefined) {
@@ -11,7 +13,9 @@ function App(props) {
       <Nav />
       <div className="container">
         <LocationForm />
-        {/* <AttendeesList attendees={props.attendees} /> */}
+        <ConferenceForm />
+        <AttendConferenceForm />
+        <AttendeesList attendees={props.attendees} />
       </div>
     </>
   );
--- ghi/app/src/LocationForm.js
+++ ghi/app/src/LocationForm.js
@@ -1,19 +1,31 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useState } from 'react';
 
-function LocationForm() {
-    const [states,setStates] = useState([]);
-    const fetchData = async () => {
-        const url = 'http://localhost:8000/api/states/';
+function LocationForm () {
+  // Here is the old way of creating state hooks for every
+  // property. Can you refactor this to make it into a single
+  // data object like the ConferenceForm() above?
+  const [name, setName] = useState('');
+  const [city, setCity] = useState('');
+  const [roomCount, setRoomCount] = useState('');
+  const [state, setState] = useState('')
+  const [states, setStates] = useState([]);
 
-        const response = await fetch(url);
+  const fetchData = async () => {
+    const url = 'http://localhost:8000/api/states/';
 
-        if (response.ok) {
-            const data = await response.json();
-            setStates(data.states)
+    const response = await fetch(url);
+
+    if (response.ok) {
+      const data = await response.json();
+      setStates(data.states);
     }
   }
 
-    const handleSubmit = async (event) => {
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const handleSubmit = async (event) => {
     event.preventDefault();
 
     const data = {};
@@ -21,7 +33,6 @@ function LocationForm() {
     data.name = name;
     data.city = city;
     data.state = state;
-    console.log(data);
 
     const locationUrl = 'http://localhost:8000/api/locations/';
     const fetchConfig = {
@@ -31,7 +42,6 @@ function LocationForm() {
         'Content-Type': 'application/json',
       },
     };
-
     const response = await fetch(locationUrl, fetchConfig);
     if (response.ok) {
       const newLocation = await response.json();
@@ -39,89 +49,68 @@ function LocationForm() {
 
       setName('');
       setRoomCount('');
-      setCity('')
+      setCity('');
       setState('');
     }
   }
 
-
-  const [name, setName] = useState('');
-
+  // How can we refactor these handleChange methods to make
+  // a single method, like the ConferenceForm above?
   const handleNameChange = (event) => {
     const value = event.target.value;
     setName(value);
   }
 
-  const [roomCount, setRoomCount] = useState('');
-
   const handleRoomCountChange = (event) => {
     const value = event.target.value;
     setRoomCount(value);
-}
-
-  const [city, setCity] = useState('');
+  }
 
   const handleCityChange = (event) => {
     const value = event.target.value;
     setCity(value);
-}
-
-  const [state, setState] = useState('');
+  }
 
   const handleStateChange = (event) => {
     const value = event.target.value;
     setState(value);
-}
+  }
 
-  useEffect(() => {
-    fetchData();
-  }, []);
-
-  // return method...
-  return(
-    <>
-    <p>A location form</p>
+  return (
     <div className="row">
       <div className="offset-3 col-6">
         <div className="shadow p-4 mt-4">
           <h1>Create a new location</h1>
           <form onSubmit={handleSubmit} id="create-location-form">
             <div className="form-floating mb-3">
-            <input value={name} onChange={handleNameChange} placeholder="Name" required
-                type="text" name="name" id="name"
-                className="form-control"  />
-                <label htmlFor="name">Name</label>
+              <input value={name} onChange={handleNameChange} placeholder="Name" required type="text" name="name" id="name" className="form-control" />
+              <label htmlFor="name">Name</label>
             </div>
             <div className="form-floating mb-3">
-            <input value={roomCount} onChange={handleRoomCountChange} placeholder="Room count"
-                required type="number" name="room_count" id="room_count"
-                className="form-control" />
-                <label htmlFor="room_count">Room Count</label>
+              <input value={roomCount} onChange={handleRoomCountChange} placeholder="Room count" required type="number" name="room_count" id="room_count" className="form-control" />
+              <label htmlFor="room_count">Room count</label>
             </div>
             <div className="form-floating mb-3">
-            <input value={city} onChange={handleCityChange} placeholder="City"
-                required type="name" name="city" id="city"
-                className="form-control" />
-                <label htmlFor="city">City</label>
+              <input value={city} onChange={handleCityChange} placeholder="City" required type="text" name="city" id="city" className="form-control" />
+              <label htmlFor="city">City</label>
             </div>
             <div className="mb-3">
-                <select value={state} onChange={handleStateChange} required name="state" id="state" className="form-select">
-                    <option value="">Choose a state</option>
-                    {states.map(state => {
-                        return (
-                            <option key={state.abbreviation} value={state.abbreviation}>
-                                {state.name}
-                            </option>
-                        );
-                    })}
-                </select>
+              <select value={state} onChange={handleStateChange} required name="state" id="state" className="form-select">
+                <option value="">Choose a state</option>
+                {states.map(state => {
+                  return (
+                    <option key={state.abbreviation} value={state.abbreviation}>
+                      {state.name}
+                    </option>
+                  );
+                })}
+              </select>
             </div>
             <button className="btn btn-primary">Create</button>
           </form>
         </div>
       </div>
     </div>
-    </>
   );
 }
 
